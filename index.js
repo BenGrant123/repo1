aedbbd22a6dfdca9aa8abd1b470bc39d9c601e5a
BenGrant123 <bengrant123@gmail.com>
2016-11-21 12:17:15 +0000

    Commit

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var app = express();
-//var bodyParser = require('body-parser'); 
-//app.use(bodyParser.urlencoded({extended: true})); 
-//app.use(bodyParser.json());
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json());
 
-app.use('/app', express.static(__dirname + '/app'));
-
-app.use('/api', function (req, res) {
-	//var userData = req.body
-	//console.log("userData is "+userData)
+app.post('/submit-survey', function (req, res) {
+	var userData = req.body
+	Object.keys(userData).forEach(function(key){
+		console.log(key + ": " + userData[key])
+	});
  	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,POST');
-	res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With')
-	res.send("success")
+	res.header('Access-Control-Allow-Methods', 'POST');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+	res.send("success");
 });
 
-var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.use('/', express.static(__dirname + '/app'));
+
+var server = app.listen(process.env.PORT || 3000);
 
